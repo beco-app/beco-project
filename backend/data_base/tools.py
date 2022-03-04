a8a5780e6d33339c623e024c7cfd8a558d776092
wind-yk <2389669757@qq.com>
2022-03-04 16:33:28 +0100

    first implementation complete

--- backend/data_base/tools.py
+++ backend/data_base/tools.py
@@ -17,10 +17,6 @@ db_promotions = config["db_promotions"]
 db_active_promotions = config["db_active_promotions"]
 db_products = config["db_products"]
 
-
-db_handler.hello()
-
-
 # Setters and Getters
 
 def getUserIdByUserName(user_name):
@@ -39,81 +35,124 @@ def getAllRecords(collection):
 def getUser(attributes=None, **query):
     """
     Return a list of records with `attributes` based on `query`.
+    Always returns '_id' if possible.
     Examples:
+        >>> getUser()
+        [...all users...]
 
-        > getUser(age=20)
+        >>> getUser(age=20)
         [{all parameters user1}, {...}, ...]
 
-        > getUser(['_id', 'email'], username='yikai')
+        >>> getUser(['_id', 'email'], username='yikai')
         [{'_id': ObjectId('...'), 'email': 'yikai.qiu@upc.edu'}]
 
-        > getUser(['_id'], age=20)
-        [{'_id', OjectId(1..)}, {'_id', OjectId(2..)}, ...]
+        >>> getUser(['email'], age=20)
+        [{'_id': OjectId(1..), 'email':...}, {'_id': OjectId(2..), emial:...}, ...]
 
     To implement:
         * Ensure that the names of the parameters are well-defined
-        * Add a `limit` parameter to limit the size of the output 
+        * Multifunctional query that supports as >, <, <=, ...
+        * Add a `limit` parameter to limit the size of the output
             (the parameter is already in db_handler.py file)
     """
     operation = dict([(attr,1) for attr in attributes]) if attributes is not None else None
     response = db_handler.queryFind(db_name, db_users, query, operation)
     return list(response)
 
-print(getUser(age=20))
 
-
-def getShop(id): 
+def getShop(attributes=None, **query):
     """
-    gets the shop as a dictionary from the id of type ObjectId
+    Each record of shop has the following attributes:
+        shopname, description, timetable, photo, location, type, product_list, phone
+    Return a list of records with `attributes` based on `query`.
+    Examples (see `tools.getUser()`)
+    To implement:
+        * Ensure that the names of the parameters are well-defined
+        * Multifunctional query that supports as >, <, <=, ...
+        * Add a `limit` parameter to limit the size of the output
+            (the parameter is already in db_handler.py file)
     """
-    pass
+    operation = dict([(attr,1) for attr in attributes]) if attributes is not None else None
+    response  = db_handler.queryFind(db_name, db_shops, query, operation)
+    return list(response)
 
 
-def getPromotion(id): 
+def getPromotion(attributes=None, **query): 
     """
-    gets the promotion as a dictionary from the id of type ObjectId
     """
-    pass
+    operation = dict([(attr,1) for attr in attributes]) if attributes is not None else None
+    response  = db_handler.queryFind(db_name, db_promotions, query, operation)
+    return list(response)
 
-def getActive_Promotion(id): 
+def getActivePromotion(attributes=None, **query): 
     """
     gets the active promotion as a dictionary from the id of type ObjectId
     """
+    operation = dict([(attr,1) for attr in attributes]) if attributes is not None else None
+    response  = db_handler.queryFind(db_name, db_active_promotions, query, operation)
+    return list(response)
     pass
 
 
 def setUser(username, email, password, phone, gender, age, zip_code, diet, becoins): 
     """
     Insert a user in the collection users.
-    The arguments i
+    The arguments are wrote explicitely in the function to avoid typos in the fields.
+    The attribute `_id` cannot be manually set.
+    
+    Returns: a tuple 
+        - (True, ObjectId) if the insertion succeed
+        - (False, None) otherwise
     """
+
+    if getUser(['_id'], username=username): # there is only one username per user
+        return (False, None)
+
     document = {
         'username': username,  'email': email, 'password': password, 'phone': phone, 
         'gender'  : gender,      'age': age,   'zip_code': zip_code, 'diet' : diet, 
         'becoins' : becoins
     }
+    response = db_handler.queryInsert(db_name, db_users, document, one=True)
+    return response.acknowledged, response.inserted_id
 
-    db_handler.queryInsert(db_name, db_users, document, one=True)
-    pass
 
-
-def setShop(): 
+def setShop(shopname, description, timetable, photo, location, _type, product_list, phone): 
     """
     inserts a shop in shops
     """
-    pass
+    
+    document = {
+        'shopname': shopname,  'description': description, 'timetable': timetable,    'photo': photo, 
+        'location': location,         'type': _type,    'product_list': product_list, 'phone': phone
+    }
+    response = db_handler.queryInsert(db_name, db_shops, document, one=True)
+    return response.acknowledged, response.inserted_id
 
 
-def setPromotion(): 
+def setPromotion(shopid, description, becoins, valid_interval): 
     """
     inserts a promotion in the table promotion
     """
-    pass
+    document = {
+        'shopid'   : shopid,  'description'   : description, 
+        'timetable': becoins, 'valid_interval': valid_interval
+    }
+    response = db_handler.queryInsert(db_name, db_promotions, document, one=True)
+    return response.acknowledged, response.inserted_id
 
 
-def setActive_Promotion(): 
+def setActivePromotion(prom_id, user_id, valid_until): 
     """
     inserts an active promotion in the table active_promotion
     """
-    pass
+    document = {
+        'prom_id': prom_id,  'user_id': user_id, 'valid_until': valid_until
+    }
+    response = db_handler.queryInsert(db_name, db_active_promotions, document, one=True)
+    return response.acknowledged, response.inserted_id
 
+if __name__ == '__main__':
+    # print('main')
+    print(getShop())
+    # print(setShop('Perruqueria Casas', '404', '', '', [40.3879, 2.16992], 'Barber', [], '600100290'))
