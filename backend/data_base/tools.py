4f5f159857ad72ca2cb492aac148fd5c0f92c7e7
wind-yk <2389669757@qq.com>
2022-03-04 10:16:08 +0100

    starting getting and setters

---
+++ backend/data_base/tools.py
@@ -0,0 +1,49 @@
+import pymongo
+import db_handler
+
+# Setters and Getters
+
+def getUser(id):
+    """gets the user as a dictionary from the id of type ObjectId"""
+
+
+def getShop(id): 
+    """
+    gets the shop as a dictionary from the id of type ObjectId
+    """
+
+
+def getPromotion(id): 
+    """
+    gets the promotion as a dictionary from the id of type ObjectId
+    """
+
+def getActive_Promotion(id): 
+    """
+    gets the active promotion as a dictionary from the id of type ObjectId
+    """
+
+
+def setUser(...): 
+    """
+    inserts a user in the table users.
+    """
+
+
+def setShop(...): 
+    """
+    inserts a shop in shops
+    """
+
+
+def setPromotion(...): 
+    """
+    inserts a promotion in the table promotion
+    """
+
+
+def setActive_Promotion(...): 
+    """
+    inserts an active promotion in the table active_promotion
+    """
+
