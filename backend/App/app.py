26bc589f9b7701e4bfac227bdeb6482b9a4c32a1
Ubuntu <ubuntu@ip-172-31-45-191.us-east-2.compute.internal>
2022-03-15 09:22:44 +0000

    Endpoints connected to DB

--- backend/App/app.py
+++ backend/App/app.py
@@ -1,10 +1,22 @@
+import sys
+# sys.path.append("/home/ubuntu/beco-project/backend")
+print(sys.version)
+from data_base import tools
+
+
 from flask import Flask
 app = Flask(__name__)
 
 # Test
 @app.route('/')
 def hello_world():
-  return 'Hello from Flask!'
+    return 'Hello from Flask!'
+
+import os
+
+@app.route('/dbfetch_test')
+def getfromDB():
+    return str(tools.getUser()[0])
 
 if __name__ == '__main__':
-  app.run()
+    app.run()
