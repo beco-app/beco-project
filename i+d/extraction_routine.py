8927efae3276a50e5da1e97563144ca54bf6f3cc
Pau Matas <pmatasalbiol@gmail.com>
2022-03-22 09:12:09 +0100

    Geo queries available

--- i+d/extraction_routine.py
+++ i+d/extraction_routine.py
@@ -5,25 +5,50 @@ from tqdm import tqdm
 
 token = '5ba0f3024f257b103b04cc3fd569a67994c57e2e'
 
-def sensors_api_query(token, lat1, lng1, lat2, lng2):
-    latlng = f'{lat2},{lng2},{lat1},{lng1}'
+def sensors_api_geo_query(token, lat, lng):
+    link = f'https://api.waqi.info/feed/geo:{lat};{lng}/?token={token}'
+
+    file = requests.get(link)
+    return json.loads(file.text)
+
+def sensors_api_latlng_query(token, lat1, lng1, lat2, lng2):
+    latlng = f'{lat1},{lng1},{lat2},{lng2}'
     link = f'https://api.waqi.info/map/bounds/?token={token}&latlng={latlng}'
 
     file = requests.get(link)
     return json.loads(file.text)
 
+def geo_query_to_latlng_style(response):
+
+    response['data']['station'] = {
+        'time': response['data']['time']['iso'],
+        'name': response['data']['city']['name']
+    }
+    response['data']['uid'] = response['data']['idx']
+    response['data'] = [response['data']]
+
+    return response
+
+def sensors_api_query(token, lat1, lng1, lat2=None, lng2=None):
+
+    return (
+        geo_query_to_latlng_style(sensors_api_geo_query(token, lat1, lng1))
+        if lat2 is None and lng2 is None
+        else sensors_api_latlng_query(token, lat1, lng1, lat2, lng2)
+    )
+
 def api_response_parsing(response):
     extraction_json = [
         {
-            'uid':station['uid'],
+            'uid':str(station['uid']),
             'aqi':float(station['aqi']),
             'time':station['station']['time']
         }
         for station in response['data']
         if station['aqi'] != '-'
     ]
-    if not extraction_json:
-        print('Nothing to extract...')
+    # if not extraction_json:
+    #     print('Nothing to extract...')
 
     with open('./stations.json') as json_file:
         stations = json.load(json_file)
@@ -45,6 +70,11 @@ def update_all_stations():
         bounding_boxes = json.load(json_file)
 
     for bb in tqdm(bounding_boxes):
+        if not 'lat2' in bb:
+            bb['lat2'] = None
+        if not 'lng2' in bb:
+            bb['lng2'] = None
+
         api_response_parsing(sensors_api_query(
             token, bb['lat1'], bb['lng1'], bb['lat2'], bb['lng2']
             ))
--- i+d/new_bounding_box.py
+++ i+d/new_bounding_box.py
@@ -8,20 +8,20 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-n", "--bb_name")
 parser.add_argument("-lat1", "--lat1", type=float)
 parser.add_argument("-lng1", "--lng1", type=float)
-parser.add_argument("-lat2", "--lat2", type=float)
-parser.add_argument("-lng2", "--lng2", type=float)
+parser.add_argument("-lat2", "--lat2", type=float, default=None, required=False)
+parser.add_argument("-lng2", "--lng2", type=float, default=None, required=False)
 
 args = parser.parse_args()
 
 extraction = sensors_api_query(token, args.lat1, args.lng1, args.lat2, args.lng2)
 
-if extraction['status'] == 'ok':
+if 'status' in extraction and extraction['status'] == 'ok':
     with open('./stations.json', 'r') as json_file:
         stations = json.load(json_file)
         for st in extraction['data']:
             stations[st['uid']] = st['station']
     with open('./stations.json', 'w') as json_file:
-        json.dump(stations, json_file, ensure_ascii=False)
+        json.dump(stations, json_file, ensure_ascii=False, indent=2)
 
     with open('./bounding_boxes.json', 'r') as json_file:
         bounding_boxes = json.load(json_file)
