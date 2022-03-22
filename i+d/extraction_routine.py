86afccd8761863bae71c6aa6cf90b5aab0c03ed3
Pau Matas <pmatasalbiol@gmail.com>
2022-03-22 11:51:47 +0100

    Extraction procedure commented + crontab problems solved

--- i+d/extraction_routine.py
+++ i+d/extraction_routine.py
@@ -1,24 +1,65 @@
+# -*- extraction_routine.py -*-
+
+"""
+Python script designed to update the stations file with data extracted form the
+air quality index api ('https://api.waqi.info/').
+It is thought to be executed daily with an external programming tool. In our
+case we use 'crontab' with a command '0 */8 * * *'.
+
+It has also isolated functions able to query the api without updating the
+stations file so it can be used as a module.
+"""
+
+# Libs
 import requests
 import json
 import pandas as pd
 from tqdm import tqdm
+import datetime
 
+__author__ = 'Pau Matas'
+__maintainer__ = 'Pau Matas'
+__email__ = 'pmatasalbiol@gmail.com'
+__status__ = 'Dev'
+
+# Global variables
 token = '5ba0f3024f257b103b04cc3fd569a67994c57e2e'
+FILES_PATH = "/Users/pau_matas/Desktop/GCED/Q6/PE/beco-project/i+d/"
 
-def sensors_api_geo_query(token, lat, lng):
-    link = f'https://api.waqi.info/feed/geo:{lat};{lng}/?token={token}'
+def sensors_api_query(token, lat1, lng1, lat2=None, lng2=None):
+    """ Given a token and one bounding box or point parameters returns the
+        response for its query to the api.
+    """
 
-    file = requests.get(link)
-    return json.loads(file.text)
+    return (
+        geo_query_to_latlng_style(sensors_api_geo_query(token, lat1, lng1))
+        if lat2 is None and lng2 is None
+        else sensors_api_latlng_query(token, lat1, lng1, lat2, lng2)
+    )
 
 def sensors_api_latlng_query(token, lat1, lng1, lat2, lng2):
+    """ Given a token and one bounding box parameters returns the response for
+        its latlng query to the api.
+    """
     latlng = f'{lat1},{lng1},{lat2},{lng2}'
     link = f'https://api.waqi.info/map/bounds/?token={token}&latlng={latlng}'
 
     file = requests.get(link)
     return json.loads(file.text)
 
+def sensors_api_geo_query(token, lat, lng):
+    """ Given a token and one geographic point parameters returns the response
+        for its geo query to the api.
+    """
+    link = f'https://api.waqi.info/feed/geo:{lat};{lng}/?token={token}'
+
+    file = requests.get(link)
+    return json.loads(file.text)
+
 def geo_query_to_latlng_style(response):
+    """ Given geo query response returns it with the latlng query response
+        dictionary structure.
+    """
 
     response['data']['station'] = {
         'time': response['data']['time']['iso'],
@@ -29,15 +70,11 @@ def geo_query_to_latlng_style(response):
 
     return response
 
-def sensors_api_query(token, lat1, lng1, lat2=None, lng2=None):
-
-    return (
-        geo_query_to_latlng_style(sensors_api_geo_query(token, lat1, lng1))
-        if lat2 is None and lng2 is None
-        else sensors_api_latlng_query(token, lat1, lng1, lat2, lng2)
-    )
-
 def api_response_parsing(response):
+    """ Given a latlng query response updates 'stations.json' file with the air
+        quality index data extracted.
+    """
+    # Select stations with new data
     extraction_json = [
         {
             'uid':str(station['uid']),
@@ -47,29 +84,43 @@ def api_response_parsing(response):
         for station in response['data']
         if station['aqi'] != '-'
     ]
-    # if not extraction_json:
-    #     print('Nothing to extract...')
+    if not extraction_json:
+        print('Nothing to extract...')
+        return
 
-    with open('./stations.json') as json_file:
+    with open(f"{FILES_PATH}stations.json") as json_file:
         stations = json.load(json_file)
 
     stations = update_aqi(extraction_json, stations)
 
-    with open('./stations.json', 'w') as json_file:
+    with open(f"{FILES_PATH}stations.json", 'w') as json_file:
         json.dump(stations, json_file, ensure_ascii=False, indent=2)
 
 def update_aqi(extraction_json, stations):
+    """ Given a extraction and the current stations information, updates and
+        returns the stations dictionary.
+    """
     for e in extraction_json:
-        stations[e['uid']]['aqi'] = e['aqi']
-        stations[e['uid']]['time'] = e['time']
+        if e['uid'] in stations:
+            stations[e['uid']]['aqi'] = e['aqi']
+            stations[e['uid']]['time'] = e['time']
+        else:
+            print((
+                f"Station with uid {e['uid']} does not exist in stations.json "
+                f"and is detected as the nearest or inside for a point or a "
+                f"bounding box, respectively."))
 
     return stations
 
 def update_all_stations():
-    with open('./bounding_boxes.json') as json_file:
+    """ For every bounding box geographic object queries the api and updates
+        the stations data file with the data extracted.
+    """
+    with open(f"{FILES_PATH}bounding_boxes.json") as json_file:
         bounding_boxes = json.load(json_file)
 
     for bb in tqdm(bounding_boxes):
+        print(f"--> {bb['name']}")
         if not 'lat2' in bb:
             bb['lat2'] = None
         if not 'lng2' in bb:
@@ -80,7 +131,9 @@ def update_all_stations():
             ))
 
 def main():
+    print(f"\tExecution at {datetime.datetime.now()}:")
     update_all_stations()
+    print('\n\n')
 
 if __name__ == '__main__':
     main()
