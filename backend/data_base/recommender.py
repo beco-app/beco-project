4135e2a753075ce210cb558e512561aa657e045e
Tomás Gadea <01tomas.gadea@gmail.com>
2022-04-20 20:28:18 +0200

    some prints to debug

--- backend/data_base/recommender.py
+++ backend/data_base/recommender.py
@@ -1,10 +1,12 @@
 import sys
+
 sys.path.append("./backend/data_base")
 from tools import *
 from collections import Counter
 from geopy.distance import distance
 import numpy as np
 from bson.objectid import ObjectId
+
 
 def get_shop_count(uid):
     """
@@ -14,8 +16,8 @@ def get_shop_count(uid):
     all_trans = getTransaction(['shop_id'], user_id=uid)
     all_shops = [t['shop_id'] for t in all_trans]
     count_shops = Counter(all_shops)
-    norm = sum([v**2 for v in count_shops.values()])
-    norm_shops = {k: v**2/norm for k, v in count_shops.items()}
+    norm = sum([v ** 2 for v in count_shops.values()])
+    norm_shops = {k: v ** 2 / norm for k, v in count_shops.items()}
     return norm_shops
 
 
@@ -23,7 +25,7 @@ def shop_count_sim(u_shops, v_shops):
     """
     Return the cosine similarity between 2 normalized dicts
     """
-    list1, list2 = sorted(u_shops.items()), sorted(v_shops.items()) # is sorted or not?
+    list1, list2 = sorted(u_shops.items()), sorted(v_shops.items())  # is sorted or not?
     id1 = id2 = dot = 0
     while id1 < len(list1) and id2 < len(list2):
         if list1[id1][0] == list2[id2][0]:
@@ -34,14 +36,14 @@ def shop_count_sim(u_shops, v_shops):
             id1 += 1
         else:
             id2 += 1
-    #print(dot, end='')
+    # print(dot, end='')
     return dot
 
 
 def recommend_new_user(user_id):
     u_zipcode = getUser(_id=user_id)[0]['zip_code']
     shops = getShop(['_id'], zip_code=u_zipcode)
-    shops = {s['_id']:1 for s in shops}
+    shops = {s['_id']: 1 for s in shops}
 
     for sh, score in shops.items():
         preferences = getUser(_id=user_id)[0]['preferences']
@@ -49,15 +51,13 @@ def recommend_new_user(user_id):
             if tag in preferences:
                 shops[sh] *= 1.25  # hyperparameter
 
-    
     return sorted(shops.items(), key=lambda x: -x[1])
 
 
 def recommend(user_id):
-
     # Find most similar users
     u_shops = get_shop_count(user_id)
-
+    print(u_shops)
     # To new users, recommend shops in its zip code with preferences
     if len(u_shops) == 0:
         return recommend_new_user(user_id)
@@ -70,7 +70,7 @@ def recommend(user_id):
         uv_sim = shop_count_sim(u_shops, v_shops)
         sims.append((v, uv_sim))
     sims = sorted(sims, key=lambda x: -x[1])
-    sims = sims[0:20]  #Hyperparameter
+    sims = sims[0:20]  # Hyperparameter
 
     # Find most similar shops
     shops = {}
@@ -82,10 +82,12 @@ def recommend(user_id):
                 shops[shop] += score
             else:
                 shops[shop] = score
-    
 
     # Compute approx ubication of user and ponderate by distance
-    
+    shop_ids = [s for s,_ in u_shops.items()]
+    print(shop_ids[0])
+    print(getShop([], _id=shop_ids[0]))
+    print([getShop([], _id=s) for s, _ in u_shops.items()])
     u_locs = [getShop([], _id=s)[0]['location'] for s, _ in u_shops.items()]
     lats, lons = [lat for lat, _ in u_locs], [lon for _, lon in u_locs]
     loc = np.mean(lats), np.mean(lons)
@@ -93,7 +95,7 @@ def recommend(user_id):
     dists = [distance(loc, s_loc).km for s_loc in s_locs]
     dists = np.array(dists) / max(dists)
     dist_scores = [np.pi / 2 - np.arctan(d) for d in dists]  # hyperparameter
-    shops = {sh: sc*dist_scores[i] for i, (sh, sc) in enumerate(shops.items())}
+    shops = {sh: sc * dist_scores[i] for i, (sh, sc) in enumerate(shops.items())}
 
     # Sum if user interested in shop tags
 
@@ -103,7 +105,9 @@ def recommend(user_id):
             if tag in preferences:
                 shops[sh] *= 1.25  # hyperparameter
 
-    
     return sorted(shops.items(), key=lambda x: -x[1])
 
     # Factors: visited or not, air pollution...
+
+
+print(recommend(ObjectId("625010dd05e04170d454bd52")))
