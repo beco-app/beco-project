97e2536ac8f1b526918261f5a48f701ebce28869
alvarodr21 <alvarodr21@gmail.com>
2022-04-26 10:58:11 +0200

    added photos

--- backend/data_base/populate_shops.py
+++ backend/data_base/populate_shops.py
@@ -12,6 +12,7 @@ def populate_shops(src):
     Function that populates the the shop database.
     The input is the source file path
     """
+    photo_links = json.load(open("./backend/data_base/photos.json"))['photos']
     random.seed(123456789)
     with open(src) as f:
         for row in csv.DictReader(f, skipinitialspace=True):
@@ -19,6 +20,9 @@ def populate_shops(src):
             items = [ v for k,v in list(row.items())[1:]]
 
             shopname, description, web, address, district, neighbourhood, location, zip_code, _type, tags, timetable, photo, product_list, phone = items
+            idx = random.randint(0, len(photo_links)-1) # random [0,15]
+            #photo = "https://upload.wikimedia.org/wikipedia/commons/b/b1/Missing-image-232x150.png"
+            photo = photo_links[idx]
             tags = eval(tags)
             lat, lon = eval(location)
             location = (float(lat.replace(',', '.')), float(lon.replace(',', '.')))
@@ -27,7 +31,8 @@ def populate_shops(src):
             shop = {
                 'shopname':shopname, 'description': description, 'web':web, 'timetable': timetable, 'photo':photo,
                 'location': location, 'address':address, 'district':district, 'zip_code':zip_code,
-                'neighbourhood': neighbourhood, 'type': _type, 'tags': tags, 'product_list': product_list, 'phone': phone
+                'neighbourhood': neighbourhood, 'type': _type, 'tags': tags, 'product_list': product_list, 'phone': phone,
+                'nearest_stations': None
             }
 
             print(setShop(shop))
