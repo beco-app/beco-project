ef51e209c0e46037e95a90bdce5218ee0e21ade5
jguaschmarti <jguaschmarti@gmail.com>
2022-04-29 11:12:27 +0200

    Fixed log out button

--- frontend/beco/lib/views/ProfileWidget.dart
+++ frontend/beco/lib/views/ProfileWidget.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
@@ -155,7 +156,9 @@ showAlertDialog(BuildContext context) {
   );
   Widget continueButton = TextButton(
     child: const Text("Continue"),
-    onPressed: () {
+    onPressed: () async {
+      await FirebaseAuth.instance.signOut();
+
       Navigator.of(context).pushNamedAndRemoveUntil(
         '/login/',
         (route) => false,
