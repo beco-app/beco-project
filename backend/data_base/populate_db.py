77bce5b9fff16ae8de8c5f4dfecbfa4e813d0786
Tomás Gadea <01tomas.gadea@gmail.com>
2022-05-16 18:09:24 +0200

    renaming param

--- backend/data_base/populate_db.py
+++ backend/data_base/populate_db.py
@@ -5,7 +5,7 @@ from simulate_transactions import transaction_gen
 from populate_users import populate_users
 from tools import *
 
-def populate_db(s, nu, np, nap, nt) :
+def populate_db(s, nu, np, nap, n_days) :
     print('-'*50, '-'*50, '-'*50, "inserting shops", sep='\n')
     populate_shops(s)
     print('-'*50, '-'*50, '-'*50, f"{len(getShop())} shops inserted", "inserting users", sep='\n')
@@ -15,8 +15,8 @@ def populate_db(s, nu, np, nap, nt) :
     print('-'*50, '-'*50, '-'*50, f"{len(getPromotion())} proms inserted", "inserting active proms", sep='\n')
     populate_active_prom(nap)
     print('-'*50, '-'*50, '-'*50, f"{len(getActivePromotion())} active proms inserted", "inserting transaction", sep='\n')
-    transaction_gen(n_days=nt, hard=True, n_hard=5)
+    transaction_gen(n_days=n_days, hard=True, n_hard=5)
     print('-'*50, '-'*50, '-'*50, f"{len(getTransaction())} transactions inserted", sep='\n')
 
 if __name__ == '__main__':
-    populate_db(s='backend/data_base/shops.csv', nu=100, np=100, nap=10, nt=70)
+    populate_db(s='backend/data_base/shops.csv', nu=100, np=100, nap=10, n_days=70)
